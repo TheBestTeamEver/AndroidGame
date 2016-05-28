b47deb0bd04373513f0651fbfcefc498129e8434
Ztaz <stanislav.bmstu@mail.ru>
2016-05-28 16:13:38 +0300

    #7 Комплит

--- app/src/main/java/com/thebestteamever/game/activities/GameOverActivity.java
+++ app/src/main/java/com/thebestteamever/game/activities/GameOverActivity.java
@@ -1,22 +1,31 @@
 package com.thebestteamever.game.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.thebestteamever.game.R;
 import com.thebestteamever.game.serviceapi.ServiceHelper;
+import com.thebestteamever.game.serviceapi.parcelable.LoginParams;
 import com.thebestteamever.game.serviceapi.parcelable.Score;
 
 public class GameOverActivity extends AppCompatActivity implements ServiceHelper.ScoreResultListener {
 
     private int requestId;
+    private String score;
+
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        Intent intent = getIntent();
+        score = intent.getStringExtra(GameActivity.EXTRA_MESSAGE);
 
         showScore();
         sendScoreToServer();
@@ -24,18 +33,20 @@ public class GameOverActivity extends AppCompatActivity implements ServiceHelper
 
 
     private void showScore() {
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(GameActivity.EXTRA_MESSAGE);
+
         TextView textView = (TextView) findViewById(R.id.textView7);
         assert textView != null;
-        textView.setText(message);
+        textView.setText(score);
     }
 
     private void sendScoreToServer() {
-        // TODO: send score to server
+        sharedPreferences = getSharedPreferences(LoginActivity.SAVED, Context.MODE_PRIVATE);
 
-        // requestId = ServiceHelper.makeScore(this, new Score(login, score));
+        if (sharedPreferences.contains(LoginActivity.SAVED_LOGIN)) {
 
+            String login = sharedPreferences.getString(LoginActivity.SAVED_LOGIN, "");
+            requestId = ServiceHelper.makeScore(this, new Score(login, score), this);
+        }
     }
 
     @Override
