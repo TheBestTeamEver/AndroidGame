feb371e60cc1755920662df3ed82c5c24b9f219d
Ztaz <stanislav.bmstu@mail.ru>
2016-05-28 16:01:09 +0300

    #7 Заготовка

--- app/src/main/java/com/thebestteamever/game/activities/GameOverActivity.java
+++ app/src/main/java/com/thebestteamever/game/activities/GameOverActivity.java
@@ -6,8 +6,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.thebestteamever.game.R;
+import com.thebestteamever.game.serviceapi.ServiceHelper;
+import com.thebestteamever.game.serviceapi.parcelable.Score;
 
-public class GameOverActivity extends AppCompatActivity {
+public class GameOverActivity extends AppCompatActivity implements ServiceHelper.ScoreResultListener {
+
+    private int requestId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +33,13 @@ public class GameOverActivity extends AppCompatActivity {
 
     private void sendScoreToServer() {
         // TODO: send score to server
+
+        // requestId = ServiceHelper.makeScore(this, new Score(login, score));
+
+    }
+
+    @Override
+    public void onScoreResult(boolean success, String result) {
+
     }
 }
